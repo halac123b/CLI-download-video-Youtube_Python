b0bad58823be4fb77e3a9f329ab4a6a0b434b31c
halac123b <halac123b@gmail.com>
2024-09-14 20:45:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # Check setuptools, nếu k có dùng distutils có sẵn
+
+from setuptools import setup, Command
+setuptools_available = True
+
 try:
-    from setuptools import setup, Command
-
-    setuptools_available = True
-except ImportError:
-    from distutils.core import setup, Command
-
-    setuptools_available = False
+    # This will create an exe that needs Microsoft Visual C++ 2008
+    # Redistributable Package
+    import py2exe
+except:
+    pass
